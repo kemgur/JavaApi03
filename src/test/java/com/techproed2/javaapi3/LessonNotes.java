b869ae54de0328672f3eddd5f3d25484892bf0a1
kemalgurler <kemgur52@gmail.com>
2021-06-21 21:47:50 -0400

    jun21

---
+++ src/test/java/com/techproed2/javaapi3/LessonNotes.java
@@ -0,0 +1,19 @@
+package com.techproed2.javaapi3;
+
+public class LessonNotes {
+	/*
+	 API: stands for Application Programming Interface
+	 API: is an application without User Interface(UI)
+	 API: specify how one software program should interact with other software programs 
+	 API: bir yazılım programının diğer yazılım programlarıyla nasıl etkileşime girmesi gerektiğini belirtin
+	 
+	 Assume an API as a waiter at the restaurant
+	 
+	 End to End(E2E) Scenariss:
+	 1) Create, Update, Delete a data in Database vy using User Interface then
+	 
+	 
+	 
+	 */
+
+}
